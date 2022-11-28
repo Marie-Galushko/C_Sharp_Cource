25b9f06575f59a38e3d026e6b1915a18d034b13c
Marie <marie.galushko@gmail.com>
2022-11-28 20:04:42 +0300

    HW2 Task 4

---
+++ HomeWork2/2_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write ("Введите порядковый номер дня недели: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (number<1||number>7)
+Console.WriteLine ("С таким номером дня недели не существует");
+else if (number > 5) {
+Console.WriteLine ("Ура, долгожданный выходной!");
+}
+else Console.WriteLine ("Трудовой будничный день");
