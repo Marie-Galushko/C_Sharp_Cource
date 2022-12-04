f67f0d7c3a342cb9e96cb0f962d001a598bece09
Marie <marie.galushko@gmail.com>
2022-12-04 15:51:07 +0300

    Task 3_1

---
+++ HW3/3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int chislo = int.Parse(Console.ReadLine());
+Console.WriteLine(Polindrom(chislo));
+
+string Polindrom(int N)
+{
+    int N2 = N;
+    int num = 0;
+    int reverse = 0;
+    while (N > 0)
+    {
+        num = N % 10;
+        reverse = reverse * 10 + num;
+        N = N / 10;
+    }
+    if (N2 == reverse)
+        return "Число является полиндромом";
+    else
+        return "Число не является полиндромом";
+}
+
