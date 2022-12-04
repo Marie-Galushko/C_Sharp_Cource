6ba6334f80729a825fdf489f957bc434ea647a88
Marie <marie.galushko@gmail.com>
2022-12-04 18:57:02 +0300

    Task 4_2 4_lesson

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 1;
+while (n / 10 != 0)
+{
+    n /= 10; // n = n/10
+    count++; // count = count + 1
+}
+Console.WriteLine(count);
