7f77d151536d1ddb6bc167389740865ee1b4b757
Marie <marie.galushko@gmail.com>
2022-12-07 16:51:18 +0300

    Addition to 4_2 4 lesson

--- 4_lesson/4_2/Program.cs
+++ 4_lesson/4_2/Program.cs
@@ -3,10 +3,10 @@
 
 Console.Clear();
 Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine()), count = 1;
+int n = Convert.ToInt32(Console.ReadLine()), count = 1; // count - кол-во цифр в числе
 while (n / 10 != 0)
 {
-    n /= 10; // n = n/10
-    count++; // count = count + 1
+    n /= 10; // n = n/10, отбрасываем посл. цифру числа
+    count++; // count = count + 1, увеличиваем на единицу
 }
 Console.WriteLine(count);
