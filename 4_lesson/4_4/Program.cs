6dbb76ba0dc63991ea869b44eb661ed18dd06eee
Marie <marie.galushko@gmail.com>
2022-12-07 17:22:37 +0300

    Addition to 4_4 4 lesson

--- 4_lesson/4_4/Program.cs
+++ 4_lesson/4_4/Program.cs
@@ -7,3 +7,19 @@ int[] array = new int[8];
 for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(0, 2);
 Console.WriteLine($"[{string.Join(", ", array)}]");
+
+// !!! Основной код для ввода n элементов массива в рандоме
+// 2 вариант Марии
+
+void Massiv(int n)
+	{
+	    int[] array = new int[n];
+	    for (int i = 0; i < n; i++)
+	    {
+	        array[i] = new Random().Next(2);
+	        Console.Write($"{array[i]}, ");
+	    }   
+	}
+	
+	Console.WriteLine("Введите число элементов массива");
+	Massiv(int.Parse(Console.ReadLine()));
