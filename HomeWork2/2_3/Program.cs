57ea7ae55c68d57f5499b66cb0e9f7d28f2072f2
Marie <marie.galushko@gmail.com>
2022-12-07 15:44:11 +0300

    2nd variant of Task 2_3 of Homework2

--- HomeWork2/2_3/Program.cs
+++ HomeWork2/2_3/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Напишите программу, которая выводит третью цифру
 // заданного числа или сообщает, что третьей цифры нет
 
+// 1 вариант
+
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine(ThirdDigit(number));
@@ -19,3 +21,20 @@ string ThirdDigit(int num)
         return "Данное число меньше трехзначного, третья цифра в нем отсутствует";
 }
 
+// 2 вариант
+
+// ДЗ 2. task 13
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("3-ей цифры нет");
+else
+{
+    while (n > 1000)
+        n = n / 10;
+    
+    Console.WriteLine(n);
+    Console.WriteLine(n % 10);
+}
