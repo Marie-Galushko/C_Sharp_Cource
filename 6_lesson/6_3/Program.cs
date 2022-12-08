62cd5b5b992299cf8d1f684b926e3f9c139e9ad5
Marie <marie.galushko@gmail.com>
2022-12-08 20:35:05 +0300

    Task 6_3 6 lesson

---
+++ 6_lesson/6_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+while (n > 0)
+{
+    result = Convert.ToString(n % 2) + result;
+    n /= 2;
+}
+Console.WriteLine(result);
