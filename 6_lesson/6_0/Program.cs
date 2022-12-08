987147fd78ff693b229be5f626386ad88dfee120
Marie <marie.galushko@gmail.com>
2022-12-08 20:15:23 +0300

    Генерация случайных дробных чисел 6 lesson

---
+++ 6_lesson/6_0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+// Генерация случайных дробных чисел
+//                                        * (end - begin) + begin
+double number = (new Random().NextDouble() * (50 - 10) + 10, 2); // [0, 1] 
+// ... + 10, 2) - 2 знака после запятой
+// [begin; end]
+Console.WriteLine(number);
