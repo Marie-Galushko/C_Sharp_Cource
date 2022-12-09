8aff7d52ebfc6881cf3d13fc7d701f992f2cc888
Marie <marie.galushko@gmail.com>
2022-12-09 12:29:09 +0300

    Var 2 Task 5_1 5 lesson

--- 5_lesson/5_1/Program.cs
+++ 5_lesson/5_1/Program.cs
@@ -1,32 +1,57 @@
 ﻿//Напишите программу замены элементов массива: положительные элементы 
 //замените на соответствующие отрицательные, и наоборот.
 
-void Print(int[] arr)
+void InputArray(int[] array) //InputArray - процедура для заполнения нашего массива, внутри процедуры передаем сам наш массив array
 {
-    int size = arr.Length;
-
-    for (int i = 0; i < size; i++)
-        Console.Write($"{arr[i]} ");
-    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-9, 10);
 }
 
-int[] Mass(int size)
-{
-    int[] arr = new int[size];
+void ReleaseArray(int[] array) // процедура решения задачи
 
-    for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(-100, 100);
-    return arr;
+{
+    for (int i = 0; i < array.Length; i++) // проходимся по каждому эл
+        array[i] *= (-1); // array[i] = array[i] * (-1)
 }
 
-void Massiv(int[] arr_1)
-{
-    for (int i = 0;i < arr_1.Length; i++)
-        arr_1[i] = -arr_1[i];
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+// спросили у пользователя, завели массив
+InputArray(array); // вызвали процедуру заполнения массива
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]"); // вывод начального массива на экран
+ReleaseArray(array); // вызвали процедуру решения задачи
+Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]"); // вывод полученного массива на экран
 
-}
+// 2 вариант
 
-int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
-Print(arr_1);
-Massiv(arr_1);
-Print(arr_1);
+// void Print(int[] arr)
+// {
+//     int size = arr.Length;
+
+//     for (int i = 0; i < size; i++)
+//         Console.Write($"{arr[i]} ");
+//     Console.WriteLine();
+// }
+
+// int[] Mass(int size)
+// {
+//     int[] arr = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//         arr[i] = new Random().Next(-100, 100);
+//     return arr;
+// }
+
+// void Massiv(int[] arr_1)
+// {
+//     for (int i = 0;i < arr_1.Length; i++)
+//         arr_1[i] = -arr_1[i];
+
+// }
+
+// int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
+// Print(arr_1);
+// Massiv(arr_1);
+// Print(arr_1);
