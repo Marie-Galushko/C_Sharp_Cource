e8aee8ed90490405f84c03377235ee7bf12cbc54
Marie <marie.galushko@gmail.com>
2022-12-12 20:45:07 +0300

    Task 7_1 7 lesson

---
+++ 7_lesson/7_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив размера m на n, каждый
+// элемент в массиве находится по формуле: Amn = m + n.
+// Выведите полученный массив на экран
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j; // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+InputMatrix(matrix);
