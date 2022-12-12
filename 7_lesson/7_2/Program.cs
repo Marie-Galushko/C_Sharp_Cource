86a7a0b864b603ae72c7b1a181927dbc452215a9
Marie <marie.galushko@gmail.com>
2022-12-12 20:58:45 +0300

    Task 7_2 7 lesson

---
+++ 7_lesson/7_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых
+// оба индекса нечётные, и замените эти элементы на их квадраты.
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальная матрица: ");
+InputMatrix(matrix);
+Console.WriteLine("Результат: ");
+ReleaseMatrix(matrix);
