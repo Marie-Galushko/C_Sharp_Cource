df300efc6e4f69696624b485c48d724d709eb914
Marie <marie.galushko@gmail.com>
2022-12-17 16:40:35 +0300

    HW6_2 Var 2, 3

--- 6_lesson/HW6_2/Program.cs
+++ 6_lesson/HW6_2/Program.cs
@@ -2,19 +2,33 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
+Console.Clear();
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2= Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Значение x = {(b2 - b1) / (k1 - k2)}");
+Console.WriteLine($"Значение y = {k1 * (b2 - b1) / (k1 - k2) + b1}");
+
+// 2 вариант
+
+// Console.Write("Введите k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
  
-var x = (b2 - b1) / (k1 - k2);
-var y = k1 * x + b1;
+// var x = (b2 - b1) / (k1 - k2);
+// var y = k1 * x + b1;
  
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
+// x = Math.Round(x, 3);
+// y = Math.Round(y, 3);
  
-Console.WriteLine($"Точка пересечения с координатами: ({x};{y})");
+// Console.WriteLine($"Точка пересечения с координатами: ({x};{y})");
