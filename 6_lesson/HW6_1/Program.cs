f3fbf0c6d672093d26a88306d71b600af28a3cef
Marie <marie.galushko@gmail.com>
2022-12-17 16:36:44 +0300

    HW6 Var 2, 3

--- 6_lesson/HW6_1/Program.cs
+++ 6_lesson/HW6_1/Program.cs
@@ -6,4 +6,29 @@ int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = arr.Count(x=> x > 0);
 Console.WriteLine($"Кол-во элементов больше нуля: {count}");
 
+// 2 вариант
+
+// Console.Clear();
+// Console.Write("Введите кол-во чисел: ");
+// int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+// for (int i = 0; i < n; i++)
+// {
+//     int x = Convert.ToInt32(Console.ReadLine());
+//     if (x > 0)
+//         count++;
+// }
+// Console.WriteLine(count);
+
+// 3 вариант
+
+// Console.Clear();
+// int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// int count = 0;
+// foreach (int element in numbers)
+// {
+//     if (element > 0)
+//         count++;
+// }
+// Console.WriteLine(count);
+
 
