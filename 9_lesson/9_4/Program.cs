62db590a2c361cab9329e0bd65d3c15e5fffaa06
Marie <marie.galushko@gmail.com>
2022-12-19 21:14:31 +0300

    9_lesson task 9_4

---
+++ 9_lesson/9_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+
+int rec(int a)
+{
+    if (a / 10 == 0 || a < 10 || a % 10 == a)
+        return a;
+    return rec(a / 10) + a % 10;
+}
+
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
