b6e9c0f5441d8d44d9ee0bc0bb910f0e37627cbf
Marie <marie.galushko@gmail.com>
2022-12-21 15:54:51 +0300

    9_lesson HW9_1

---
+++ 9_lesson/HW9_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все четные натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void EvenNum(int a)
+{
+    if(a == 0) return;
+    if(a%2 == 0)
+    EvenNum(a-2);
+    else
+    EvenNum(a-1);
+    if(a%2 == 0)
+    Console.Write($" {a} ");
+
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+EvenNum(n);
